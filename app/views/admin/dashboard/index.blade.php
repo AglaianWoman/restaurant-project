63bdea304afdca4bdcbc396ed6b4eb14935fb2e2
YOUR NAME <YOUR@EMAIL.com>
2015-12-26 15:34:53 -0800

    new text for dashboard

--- app/views/admin/dashboard/index.blade.php
+++ app/views/admin/dashboard/index.blade.php
@@ -1,9 +1,10 @@
 @extends("layouts.admin")
 @section("content")
 
-Put newly added restaurants here
-
-put high rated restaurants
+Put newly added restaurants here the top 10
+<br>
+put high rated restaurants here the top 10
+put most visited restaurants here top 10
 
 
 @stop
