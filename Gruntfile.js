9dc0ec8452fb7a8b28e00ff9c9b456e2063ef110
YOUR NAME <YOUR@EMAIL.com>
2016-02-06 18:25:26 -0800

    using grunt to compile css files and editing main layout for admin

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+module.exports = function(grunt) {
+	
+	grunt.initConfig({
+		
+		
+		bower_concat: {
+			all: {
+			
+				dest:"build/_bower.js",
+				cssDest: "build/_bower.css",
+			mainFiles: {
+				'bootstrap': ['dist/css/bootstrap.css']
+			}
+		}
+		},
+		
+		/*START: CONCAT*/
+		concat: {
+			options: {
+				process: function(src,filepath) {
+					return "\n/**************/\n/****"+
+					filepath+"*****/\n/***********/\n"+src;
+					
+				}
+			},
+			//concatenate files from bower
+			bower: {
+				files : {
+					"public/css/_bower.css": 
+						["bower_components/bootstrap/dist/css/*.min.css",
+					     "bower_components/dropzone/dist/*.css",
+					     "bower_components/font-awesome/css/font-awesome.css",
+					     ],
+					     "public/js/_bower.js":[
+                    	   "bower_components/jquery/dist/*.min.js",         
+                    	   "bower_components/angular/angular.js",
+                           "bower_components/bootstrap/dist/js/*.min.js",
+                           "bower_components/dropzone/dist/*.min.js",
+                           ]
+				}
+			},
+			//concatenate files to be used in backend part of the website
+			admin: {
+				files: {
+					"public/css/backend.main.css": ["public/css/_bower.css",
+                    "public/css/backend/*.css"],
+                    "public/js/backend.main.js": ["public/js/_bower.js"]
+			
+				}
+			}
+		},
+		/**END: CONCAT**/
+		
+		/**STARTl COPY**/
+		copy: {
+			fonts: {
+				files: [
+					{expand:true,flatten:true, 
+						src:["bower_components/font-awesome/fonts/*"],
+						dest:"public/fonts/"},
+					{expand:true,flatten:true,
+							src:["bower_components/bootstrap/fonts/*"],
+							dest:"public/fonts/"},
+				]
+			}
+		},
+		/**END: COPY**/
+		
+		/**START: CLEAN**/
+		clean: {
+			fonts: {
+				src: ["public/fonts/*"]
+			}
+		},
+		/**END: CLEAN**/
+		
+		/**START: CSSMIN**/
+		cssmin: {
+			backend: {
+				files:[{
+					expand:true,
+					cwd: "public/css",
+					src: ["backend.main.css"],
+					dest: "public/css",
+					ext: ".min.css"
+				}] 
+				
+			}
+		},
+		/**END: CSSMIN**/
+		
+		/**START: UGLIFY**/
+		uglify: {
+			backend: {
+				files:{
+					'public/js/backend.main.min.js':['public/js/backend.main.js']
+				}
+			}
+		}
+		/**END: UGLIFY**/
+	});
+	
+	grunt.loadNpmTasks('grunt-bower-concat');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	
+	grunt.registerTask("default",["bower_concat:all"]);
+	grunt.registerTask("concat_bower_files",["concat:bower"]);
+	grunt.registerTask("copy_fonts",["clean:fonts","copy:fonts"]);
+	
+	grunt.registerTask("development",["clean:fonts",
+	                             "copy:fonts",
+	                             "concat:bower",
+	                             "concat:admin"]);
+	grunt.registerTask("minify",["cssmin:backend","uglify:backend"]);
+	
+	grunt.registerTask("production",['development','minify']);
+}
--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -1,15 +1,21 @@
 <!doctype html>
 <html>
 	<head>
-		<?php echo HTML::style('css/bootstrap.min.css'); ?>
-		<?php echo HTML::style('css/bootstrap-responsive.min.css'); ?>
-		<?php echo Html::style("css/admin.css"); ?>
-		<?php echo Html::script("js/angular.min.js"); ?>
+		<?php echo HTML::style('css/backend.main.css'); ?>
+		<?php //echo HTML::style('css/bootstrap-responsive.min.css'); ?>
+		<?php //echo Html::style("css/admin.css"); ?>
+		<?php //echo Html::script("js/angular.min.js"); ?>
+		<?php echo Html::script("js/backend.main.js"); ?>
 		<title><?php echo $title; ?></title>
 	</head>
     <body>
+    <div class="container-fluid">
+    <div class="row">
     	<h4>Admin page ><?php echo $title; ?></h4>
-       <section id="admin-sidebar">
+    </div>
+    <div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-3">
+    	<section id="admin-sidebar">
        	<ul>
        		<li><a href="<?php echo URL::to("admin");?>">Dashboard</a></li>
           <li><a href="<?php echo URL::to("admin/restaurants");?>">Restaurants</a></li>
@@ -18,8 +24,16 @@
           <li><a href="<?php echo URL::to("admin/states"); ?>">States</a></li>
        	</ul>
        </section>
+    </div>
+    <div class="col-lg-9 col-md-9 col-sm-9">
        <section id="admin-content">
         @yield('content')
     </section>
+    </div>
+    
+    	
+    	</div>
+       
+    </div>
     </body>
 </html>
